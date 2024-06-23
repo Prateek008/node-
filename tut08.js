afb767e6b9f25f3078b279b38a019ef69ca40519
Prateek008 <90400213+Prateek008@users.noreply.github.com>
2024-06-23 19:35:04 +0530

    Delete tut08.js

--- tut08.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-http.createServer((req,resp)=>{
-    resp.writeHead(200,{'Content-Type':'application\json'});
-    resp.write(JSON.stringify({name:'Prateek gupta',email:'prateek@gmail.com'}));
-    resp.end();
-}).listen(8001);
